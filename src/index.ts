342f0f21a40d3127c141bb20dbbc960ddf071f75
joko kenta <kenta.joko@vitalize.co.jp>
2024-03-01 17:34:47 +0900

    Function

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,24 @@
 // const world = new World('Hello World !!')
 // world.sayHello(root)
 
-import {anySample, notExistSample, primitiveSample, unkwonSample } from "./basic";
+// 03.基本の型定義
+// import {anySample, notExistSample, primitiveSample, unkwonSample } from "./basic";
+// primitiveSample()
+// notExistSample()
+// anySample()
+// unkwonSample()
 
-primitiveSample()
-notExistSample()
-anySample()
-unkwonSample()
+// 04.関数の型定義
+// import { isUserStudentIn, isUserStudentIn2, sumPrice } from "./function/parameters";
+// import { alwaysThrowError, logMessage, logMessage2, logMessage3, logMessage4 } from "./function/basic";
+// logMessage("Hello TypeScript")
+// logMessage2("Hello TypeScript2")
+// logMessage3("Hello TypeScript3")
+// logMessage4("Hello TypeScript4")
+// alwaysThrowError("Error")
+// isUserStudentIn("ABC", "jo")
+// isUserStudentIn("DEF")
+// isUserStudentIn2("ABC", "jo")
+// isUserStudentIn2("ABC")
+// const sum = sumPrice(1,2,3,4,5)
+// console.log(sum)
